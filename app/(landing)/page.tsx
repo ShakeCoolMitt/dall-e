ac72cd00d7616b67513648b652030ce5f2b2cbdc
Daylen Nguyen <daylennguyen@gmail.com>
2024-03-09 22:27:35 -0800

    remove garbo comments

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,21 +6,7 @@ import Hero from "@/components/ui/hero";
 
 const LandingPage = () => {
   return (
-    <>
-      {/* <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1>VectoAi</h1>
-        <div>
-          <Link href="/sign-in">
-            <Button>Login</Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button>Register</Button>
-          </Link>
-        </div>
-      </div> */}
     <Hero/>
-      {/* <Example/> */}
-    </>
   );
 };
 
