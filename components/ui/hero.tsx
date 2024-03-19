fa00c5e45e901dd5877ad35662984c912b855305
Daylen Nguyen <daylennguyen@gmail.com>
2024-03-19 05:56:43 +0000

    feat: Adding icon and heading

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -119,33 +119,17 @@ export default function Example() {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
+         
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Your ideas into reality
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Use AI to create high quality SVG images from prompts
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link href="/sign-up">Get started</Link>
-
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+              <Link className="text-sm font-semibold leading-6 text-gray-900" href="/sign-up">Get started</Link>
+              <Link className="text-sm font-semibold leading-6 text-gray-900" href="/sign-up">Learn More  <span aria-hidden="true">→</span></Link>
             </div>
           </div>
         </div>
